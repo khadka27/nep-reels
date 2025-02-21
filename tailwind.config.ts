ef4bbb7f13d8a2d47b0b7e94c3d452dafdeabc77
Khadka <120006828+khadka27@users.noreply.github.com>
2025-02-21 15:06:21 +0545

    Update Tailwind CSS configuration to reflect new directory structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import daisyui from "daisyui";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
